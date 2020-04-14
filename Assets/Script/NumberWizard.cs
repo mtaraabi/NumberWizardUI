64899f997695524e46d173dc49b5dd046bde8170
Mohammad Alharthi <mtalaraabi@gmail.com>
2020-04-15 02:07:50 +0300

    first

---
+++ Assets/Script/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+   
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    [SerializeField] TextMeshProUGUI NumberOfGuessText;
+    [SerializeField] Button higherButton;
+    [SerializeField] Button lowerButton;
+
+    int guess;
+    int numberOfTry;
+
+    void Start()
+    {
+        StartGame();
+    }
+    // start game method
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        if (guess > guess || guess < 1000)
+        {
+            guess = Random.Range(min, max + 1);
+            guessText.text = guess.ToString();
+            numberOfTry = numberOfTry + 1;
+            NumberOfGuessText.text = numberOfTry.ToString();
+        } else
+        {
+            higherButton.interactable = false;
+            lowerButton.interactable = false;
+        }
+    }
+
+    // start form here
+
+    public void OnPressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+}
